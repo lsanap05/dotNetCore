edb9507e4aec52d80d2e425f270aa40f0f001536
lsanap05 <lsanap05@gmail.com>
2020-08-25 09:59:03 +0530

    k

--- eShopping/eShopping/Repositories/CustomerRepository.cs
+++ eShopping/eShopping/Repositories/CustomerRepository.cs
@@ -10,32 +10,53 @@ namespace eShopping.Repositories
     public class CustomerRepository : IRepository<Customer, int>
     {
         private readonly ShoppingDbContext context;
+
+        public CustomerRepository(ShoppingDbContext context)
+        {
+            this.context = context;
+        }
         public async Task<Customer> CreateAsync(Customer entity)
         {
-            //var res = await context._Customer.AllAsync(entity);
-            //await context.SaveChangesAsync();
-            //return res.Entity;
-            throw new NotImplementedException();
+            var res = await context._Customer.AddAsync(entity);
+            await context.SaveChangesAsync();
+            return res.Entity;
         }
 
         public async Task<bool> DeleteAsync(int id)
         {
-            throw new NotImplementedException();
+            var cust = await context._Customer.FindAsync(id);
+            if (cust == null) return false;
+
+            context._Customer.Remove(cust);
+            context.SaveChanges();
+            return true;
         }
 
         public async Task<IEnumerable<Customer>> GetAsync()
         {
-            throw new NotImplementedException();
+            return await context._Customer.ToListAsync();
         }
 
         public async Task<Customer> GetAsync(int id)
         {
-            throw new NotImplementedException();
+            return await context._Customer.FindAsync(id);
         }
 
         public async Task<Customer> UpdateAsync(int id, Customer entity)
         {
-            throw new NotImplementedException();
+            var cat = await context._Customer.FindAsync(id);
+            if (cat != null)
+            {
+                cat.CustomerId = entity.CustomerId;
+                cat.CustomerName = entity.CustomerName;
+                cat.CustomerDOB = entity.CustomerDOB;
+                cat.CustomerEmailAddress = entity.CustomerEmailAddress;
+                cat.CustomerPhoneNumber = entity.CustomerPhoneNumber;
+                cat.CustomerAddress = entity.CustomerAddress;
+                await context.SaveChangesAsync();
+                return cat;
+            }
+            return entity;
         }
     }
 }
